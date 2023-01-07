f6f0931effc9c1052efd9764c9ea40e23a2252bc
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-07 17:26:32 -0400

    fix: Removed imports and props

--- App.tsx
+++ App.tsx
@@ -1,24 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 
 import AddCard from "./src/components/add";
-import dark from "./src/components/theme/dark";
-import ligth from "./src/components/theme/ligth";
+import dark from "./src/styles/theme/dark";
+import ligth from "./src/styles/theme/ligth";
 
 export default function App() {
   return (
     <ThemeProvider theme={ligth}>
-      <AddCard type="primary" />
+      <AddCard />
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
