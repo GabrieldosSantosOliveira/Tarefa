0e637a542e0fbde102a849d1593725ada7724ab5
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-08 18:22:37 -0400

    feat: Change ThemeProvider for NativeBaseProvider

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
+import { Loading } from '@components/Loading';
 import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { Routes } from '@routes/index';
-import { Loading } from '@screens/Loading';
-import ligth from '@styles/theme/ligth';
+import { THEME } from '@styles/theme';
 import { StatusBar } from 'expo-status-bar';
-import { ThemeProvider } from 'styled-components/native';
-
+import { NativeBaseProvider } from 'native-base';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
   });
   return (
-    <ThemeProvider theme={ligth}>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar style="auto" />
       {fontsLoaded ? <Routes /> : <Loading />}
-    </ThemeProvider>
+    </NativeBaseProvider>
   );
 }
