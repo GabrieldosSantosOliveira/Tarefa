c4de74c54c88b04f870039337644b6509b29f6b4
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-08 18:20:32 -0400

    fix: Remove folder theme

--- src/styles/theme/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-import ligth from "./ligth";
-
-declare module "styled-components" {
-  type ThemeType = typeof ligth;
-  export interface DefaultTheme extends ThemeType 
-}
