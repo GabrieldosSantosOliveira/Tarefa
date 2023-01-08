e17b031302db7291cebb3ed4a6ce0e66a4ec3f15
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-08 18:23:55 -0400

    feat: Add screen GeneratePasswordScreen

---
+++ src/screens/GeneratePassword.tsx
@@ -0,0 +1,8 @@
+import { VStack, Text } from 'native-base';
+export const GeneratePasswordScreen = () => {
+  return (
+    <VStack flex={1}>
+      <Text>NativeBaseProvider</Text>
+    </VStack>
+  );
+};
