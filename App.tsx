ae7f865439a6786dc588ce84e8a6030127b3a305
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-09 21:19:56 -0400

    feat: Using StatusBar from NativeBase

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import { Loading } from '@components/Loading';
+import { ThemeProvider } from '@context/ThemeContext';
 import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { Routes } from '@routes/index';
 import { THEME } from '@styles/theme';
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
   });
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar style="auto" />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <ThemeProvider>
+        <StatusBar translucent backgroundColor="#27272a" />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </ThemeProvider>
     </NativeBaseProvider>
   );
 }
