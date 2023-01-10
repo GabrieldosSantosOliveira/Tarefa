de06a253e1e5b98e269cf1d66fbf6c68a0606026
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-09 21:24:02 -0400

    feat: Add component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { HStack, Heading, Text } from 'native-base';
+interface HeaderProps {
+  title: string;
+}
+export const Header = ({ title }: HeaderProps) => {
+  return (
+    <HStack bg="gray.800" h="14" alignItems="center" justifyContent="center">
+      <Text>{title}</Text>
+    </HStack>
+  );
+};
