4e670a5e4f99e2cc60ce43dfa17dde906d7cfa63
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-09 21:26:47 -0400

    feat: Add default style in Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,6 +5,20 @@ const InputBase: ForwardRefRenderFunction<IInputProps, IInputProps> = (
   { ...props },
   ref,
 ) => {
-  return <NativeBaseInput ref={ref} {...props} />;
+  return (
+    <NativeBaseInput
+      rounded="md"
+      h="14"
+      w="full"
+      bg="info.900"
+      ref={ref}
+      borderWidth={0}
+      placeholderTextColor="black"
+      color="white"
+      fontSize={14}
+      _focus={{ bg: 'info.800', borderColor: 'orange.300', borderWidth: 1 }}
+      {...props}
+    />
+  );
 };
 export const Input = memo(forwardRef(InputBase));
