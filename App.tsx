0ef98fde94d54ed20e80d43e288abb2b7f35e8dd
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-13 17:57:20 -0400

    feat: Add PasswordProvider

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import { Loading } from '@components/Loading';
+import { PasswordProvider } from '@context/PasswordContext';
 import { ThemeProvider } from '@context/ThemeContext';
-import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import { Routes } from '@routes/index';
 import { THEME } from '@styles/theme';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
+    Roboto_700Bold,
   });
   return (
     <NativeBaseProvider theme={THEME}>
-      <ThemeProvider>
-        <StatusBar translucent backgroundColor="#27272a" />
-        {fontsLoaded ? <Routes /> : <Loading />}
-      </ThemeProvider>
+      <PasswordProvider>
+        <ThemeProvider>
+          <StatusBar translucent backgroundColor="#27272a" />
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </ThemeProvider>
+      </PasswordProvider>
     </NativeBaseProvider>
   );
 }
