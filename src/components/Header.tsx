d618e61eaea8f9a75e67b26295abe5d1ab701bec
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-13 20:27:24 -0400

    feat: Add button goBack

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,33 @@
-import { HStack, Text } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import { HStack, IconButton, Box, Heading } from 'native-base';
+import { CaretLeft } from 'phosphor-react-native';
 interface HeaderProps {
   title: string;
 }
 export const Header = ({ title }: HeaderProps) => {
+  const { goBack, canGoBack } = useNavigation();
+  const EmptyBoxSpace = () => <Box w={6} h={6} />;
   return (
-    <HStack bg="gray.800" h="14" alignItems="center" justifyContent="center">
-      <Text>{title}</Text>
+    <HStack
+      bg="gray.800"
+      h="14"
+      w="full"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      {canGoBack() ? (
+        <IconButton
+          icon={<CaretLeft size={30} color="white" />}
+          onPress={goBack}
+        />
+      ) : (
+        <EmptyBoxSpace />
+      )}
+
+      <Heading textAlign="center" size="sm">
+        {title}
+      </Heading>
+      <EmptyBoxSpace />
     </HStack>
   );
 };
