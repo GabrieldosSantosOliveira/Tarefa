9ae1e481e2e691e1430fc39215495a55dbb75b15
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-17 12:39:28 -0400

    Add new alias path

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
             '@hooks': './src/hooks',
             '@services': './src/services',
             '@helpers': './src/helpers',
+            '@validations': './src/validations',
           },
         },
       ],
