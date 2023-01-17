02b5b9b31c5a184443d7f9e89db0a9c0fba73edd
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-17 12:55:59 -0400

    feat: Update screen GeneratePassword

--- src/screens/GeneratePassword.tsx
+++ src/screens/GeneratePassword.tsx
@@ -4,11 +4,20 @@ import { Input } from '@components/Input';
 import { getRealm } from '@databases/realm';
 import { IPasswordSchema } from '@databases/schemas/PasswordSchema';
 import { Feather } from '@expo/vector-icons';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { usePassword } from '@hooks/usePassword';
 import { useNavigation } from '@react-navigation/native';
 import { generatePassword } from '@services/generatePassword';
+import { generatePasswordSchemaValidation } from '@validations/generatePassword';
 import { LinearGradient } from 'expo-linear-gradient';
-import { VStack, Text, HStack, Pressable, useToast } from 'native-base';
+import {
+  VStack,
+  Text,
+  HStack,
+  Pressable,
+  useToast,
+  FormControl,
+} from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import uuid from 'react-native-uuid';
 interface IForm {
@@ -19,7 +28,12 @@ export const GeneratePasswordScreen = () => {
   const settingsPassword = usePassword();
   const { navigate } = useNavigation();
   const toast = useToast();
-  const { handleSubmit, control } = useForm<IForm>({
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IForm>({
+    resolver: yupResolver(generatePasswordSchemaValidation),
     defaultValues: {
       application: '',
       emailOrPhone: '',
@@ -82,45 +96,51 @@ export const GeneratePasswordScreen = () => {
           }}
         />
         <HStack alignItems="center" justifyContent="flex-end">
-          <Pressable
-            flexDirection="row"
-            onPress={() => navigate('Stack', { screen: 'options' })}
-          >
+          <Pressable flexDirection="row" onPress={() => navigate('options')}>
             <Feather name="filter" size={24} color="white" />
             <Text>Opções</Text>
           </Pressable>
         </HStack>
-        <Controller
-          name="application"
-          control={control}
-          render={({ field: { onBlur, onChange, value } }) => {
-            return (
-              <Input
-                mt={4}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                placeholder="Nome do site ou app"
-              />
-            );
-          }}
-        />
-        <Controller
-          name="emailOrPhone"
-          control={control}
-          render={({ field: { onBlur, onChange, value } }) => {
-            return (
-              <Input
-                value={value}
-                onChangeText={onChange}
-                onBlur={onBlur}
-                mt={4}
-                placeholder="Usuário (Email ou telefone)"
-              />
-            );
-          }}
-        />
-
+        <FormControl isRequired isInvalid={'application' in errors}>
+          <Controller
+            name="application"
+            control={control}
+            render={({ field: { onBlur, onChange, value } }) => {
+              return (
+                <Input
+                  mt={4}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholder="Nome do site ou app"
+                />
+              );
+            }}
+          />
+          <FormControl.ErrorMessage>
+            {errors.application?.message}
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <FormControl isRequired isInvalid={'emailOrPhone' in errors}>
+          <Controller
+            name="emailOrPhone"
+            control={control}
+            render={({ field: { onBlur, onChange, value } }) => {
+              return (
+                <Input
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  mt={4}
+                  placeholder="Usuário (Email ou telefone)"
+                />
+              );
+            }}
+          />
+          <FormControl.ErrorMessage>
+            {errors.emailOrPhone?.message}
+          </FormControl.ErrorMessage>
+        </FormControl>
         <Button onPress={handleSubmit(onHandleGeneratePassword)} mt={4}>
           Salvar senha
         </Button>
