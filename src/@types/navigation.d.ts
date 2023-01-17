5e3f48b3d6e2542ab54cd4eb603cfe22baefa523
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-17 12:45:24 -0400

    feat: Update type navigation

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,12 +1,10 @@
-import { NavigatorScreenParams } from '@react-navigation/native';
-interface RootStackParamList {
-  options: undefined;
-}
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      Stack: NavigatorScreenParams<RootStackParamList>;
+      passwords: undefined;
+      options: undefined;
+      passwordDetails: { id: string };
     }
   }
 }
