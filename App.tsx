5e29cde93b0cfa844b28cb3f17f41ec84260e581
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-01-17 12:45:02 -0400

    feat: Poppins font loaded

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import { Loading } from '@components/Loading';
 import { PasswordProvider } from '@context/PasswordContext';
 import { ThemeProvider } from '@context/ThemeContext';
 import {
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
@@ -13,6 +17,8 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
+    Poppins_400Regular,
+    Poppins_700Bold,
   });
   return (
     <NativeBaseProvider theme={THEME}>
